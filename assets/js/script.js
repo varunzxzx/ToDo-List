41942a0a876eba88f239c24a8c99404681cf7049
Varun Hacker <varun995862@gmail.com>
2017-06-02 12:42:46 +0530

    Initial Commit

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+$("ul").on("click","li",function() {
+    $(this).toggleClass("completed");
+});
+
+$("ul").on("click","span",function(event) {
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+$("input").keypress(function(event) {
+    if(event.which === 13) {
+        var value = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span><i class='fa fa-trash'></i> </span>" + value + "</li>");
+    }
+});
+
+$(".fa-plus").click(function(){
+	$("input").fadeToggle();
+});
